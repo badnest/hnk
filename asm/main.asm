d0f3142725e7b53ec4c662cbe8483bb02e6762c9
badnest <ralph@riseup.net>
2023-08-19 15:20:23 -0300

    adicionado creditos na tela de titulo

--- asm/main.asm
+++ asm/main.asm
@@ -2,6 +2,9 @@ arch	sms.cpu
 endian	lsb
 insert	"../rom/hnk.sms"
 output	"../rom/hnk_br.sms"
+include	"./var.asm"
+include	"./table.asm"
+
 
 
 //--------------------------------------------------------
@@ -18,6 +21,37 @@ origin	$00d1f
 ld	hl,tilemap_199X	
 ld	de,$3a92		//posição do 199X na tela
 ld	bc,$040e		//4 linhas de 14 tiles
+
+
+
+//--------------------------------------------------------
+//créditos na tela de titulo
+//--------------------------------------------------------
+
+origin	$00ee0
+jp	injetar_creditos
+
+origin	$07f66
+injetar_creditos:
+jr	c,++		//se for texto 1p start, sair
+
+//na segunda vez, carregar creditos alternativos
+ld	hl,$0f64	
+push	af
+ld	a,(timer_hi)
+cp	$01
+jr	nz,+
+ld	hl,creditos_alt
++;pop	af
++;jp	$0ee5
+
+origin	$07fa0
+creditos_alt:
+//3 linhas de $1b tiles
+db	"    VERSAO BRASILEIRA      "
+db	"                           "
+db	"     HERBERT RIGHERS       "
+
 
 
 //--------------------------------------------------------
---
+++ asm/var.asm
@@ -0,0 +1,2 @@
+variable	timer_hi	=$d9a4
+variable	timer_lo	=$d9a3
