82bda345e06a500f093214ead37734457b8e6e0f
badnest <ralph@riseup.net>
2023-08-30 22:24:24 -0300

    adicionada engine de texto

---
+++ asm/txt_engine.asm
@@ -0,0 +1,97 @@
+origin	$07f2e
+
+//de = destino vram
+//hl = ponteiro texto
+//bc = offset pra subir/descer linhas
+
+txt_engine:
+	rst	08h		//configurar endereço vram
+	push	de		//guardar de pra calcular newline depois
+	
+	-;ld	a,(hl)		//carregar caractere do ponteiro (hl)
+	inc	hl		//aumentar ponteiro
+
+	check_acentos:
+		cp	$f8		//byte de controle?
+		jr	c,txt_normal	//se não, desenhar normal
+		jr	z,txt_cedilha	//cedilha?
+		cp	$fe
+		jr	c,txt_acento	//acento?
+		jr	z,txt_newline	//nova linha?
+
+	//caso contrário, resta o endbyte
+	txt_endbyte:
+		pop	de		//voltar stack
+		ret
+
+	txt_normal:
+		rst	20h		//desenhar caractere
+		rst	08h		//atualizar endereço vram
+		jr	-		//loop
+
+	txt_cedilha:
+		sub	$ce		//localizar cedilha
+		rst	20h		//desenhar cedilha
+		ld	bc,$003e
+		rst	30h		//descer linha
+		inc	a		//localizar perna do cedilha
+		rst	20h		//desenhar perna do cedilha
+		ld	bc,$0040
+		rst	28h		//subir linha
+		jr	-
+			
+	txt_acento:
+		sub	$cd		//localizar acento
+		ld	bc,$0042
+		rst	28h		//subir linha
+		rst	20h		//desenhar acento
+		ld	bc,$0040
+		rst	30h		//descer linha
+		jr	-
+
+	txt_newline:
+		pop	de		//pegar pos inicial da linha
+		ld	bc,$0080
+		rst	30h		//descer linha
+		push	de
+		jr	-
+
+
+origin	$7f70
+txt_cx:
+	push	hl
+	ld	hl,$dbc0
+	call	$025e
+	pop	hl
+	ld	a,(hl)
+	ld	e,a
+	inc	hl
+	ld	a,(hl)
+	ld	d,a
+	inc	hl
+	call	txt_engine
+	ret
+
+origin	$20
+	out	($be),a
+	inc	de
+	inc	de
+	ret
+
+origin	$28
+	ex	de,hl
+	sbc	hl,bc
+	ex	de,hl
+	push	af
+	rst	08h
+	pop	af
+	ret
+
+origin	$30
+	ex	de,hl
+	add	hl,bc
+	ex	de,hl
+	push	af
+	rst	08h
+	pop	af
+	ret
